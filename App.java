2703dc2813f41df8525acba34dd986b7329b5e47
FELIPE BELOMI DE OLIVEIRA CAMPOS <felipe.campos003@cs.up.edu.br>
2026-05-18 20:06:28 -0300

    segundo commit

--- App.java
+++ App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        try {
+            int myNumbers[] = { 1, 2, 3 };
+            System.out.println(myNumbers[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ponteiro fora dos limites do array!");
+        } finally {
+            System.out.println("Try Catch finalizado!");
+        }
     }
 }
