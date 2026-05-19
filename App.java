9c7be2bf1c4053bd82ae03cf9f69645fcbc160d6
FELIPE BELOMI DE OLIVEIRA CAMPOS <felipe.campos003@cs.up.edu.br>
2026-05-18 21:42:21 -0300

    ultimo commit de hoje

--- App.java
+++ App.java
@@ -1,10 +1,19 @@
+import java.io.IOException;
+
 public class App {
+
+    public void m() throws IOException {
+        throw new IOException("IOException lançada!");
+    }
+
     public static void main(String[] args) throws Exception {
         try {
-            int myNumbers[] = { 1, 2, 3 };
-            System.out.println(myNumbers[10]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Stacktrace: " + e.getMessage());
+            App app = new App();
+            app.m();
+            // int myNumbers[] = { 1, 2, 3 };
+            // System.out.println(myNumbers[10]);
+        } catch (IOException e) {
+            System.out.println("IOexception!\n" + e.getMessage());
         } catch (Exception e) {
             System.out.println("Algo deu errado!");
             e.printStackTrace();
